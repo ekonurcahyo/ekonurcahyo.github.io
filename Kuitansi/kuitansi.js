c640eede3a080b61951d3b1a582e9f5697256f17
ekonurcahyo <58285346+ekonurcahyo@users.noreply.github.com>
2019-11-28 16:48:28 +0700

    Kwitansi
    Kwitansi

---
+++ Kuitansi/kuitansi.js
@@ -0,0 +1,67 @@
+function terbilang() {
+    var bilangan = document.getElementById("nilai").value;
+    var kalimat = "";
+    var angka = new Array('0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
+    var kata = new Array('', 'Satu', 'Dua', 'Tiga', 'Empat', 'Lima', 'Enam', 'Tujuh', 'Delapan', 'Sembilan');
+    var tingkat = new Array('', 'Ribu', 'Juta', 'Milyar', 'Triliun');
+    var panjang_bilangan = bilangan.length;
+
+    if (panjang_bilangan > 15) {
+        kalimat = "Terlalu Banyak";
+    } else {
+        for (i = 1; i <= panjang_bilangan; i++) {
+            angka[i] = bilangan.substr(-(i), 1);
+        }
+
+        var i = 1;
+        var j = 0;
+
+        while (i <= panjang_bilangan) {
+            subkalimat = "";
+            kata1 = "";
+            kata2 = "";
+            kata3 = "";
+
+            if (angka[i + 2] != "0") {
+                if (angka[i + 2] == "1") {
+                    kata1 = "Seratus";
+                } else {
+                    kata1 = kata[angka[i + 2]] + " Ratus";
+                }
+            }
+
+            if (angka[i + 1] != "0") {
+                if (angka[i + 1] == "1") {
+                    if (angka[i] == "0") {
+                        kata2 = "Sepuluh";
+                    } else if (angka[i] == "1") {
+                        kata2 = "Sebelas";
+                    } else {
+                        kata2 = kata[angka[i]] + " Belas";
+                    }
+                } else {
+                    kata2 = kata[angka[i + 1]] + " Puluh";
+                }
+            }
+
+            if (angka[i] != "0") {
+                if (angka[i + 1] != "1") {
+                    kata3 = kata[angka[i]];
+                }
+            }
+
+            if ((angka[i] != "0") || (angka[i + 1] != "0") || (angka[i + 2] != "0")) {
+                subkalimat = kata1 + " " + kata2 + " " + kata3 + " " + tingkat[j] + " ";
+            }
+
+            kalimat = subkalimat + kalimat;
+            i = i + 3;
+            j = j + 1;
+        }
+
+        if ((angka[5] == "0") && (angka[6] == "0")) {
+            kalimat = kalimat.replace("Satu Ribu", "Seribu");
+        }
+    }
+    document.getElementById("terbilang").innerHTML = kalimat;
+}
